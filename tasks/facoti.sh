de5463a7fba5c13fb90575221d8f97ebb525231e
Brice Texier <brice.texier@ekylibre.org>
2008-06-05 05:53:33 +0000

    git-svn-id: https://www.fdsea33.fr/svn/gestsea@70 06a6e951-9446-0410-82e0-8ca84c92daee

--- tasks/facoti.sh
+++ tasks/facoti.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 d=/usr/local/bin/scripts
 dir=/home/groupes/toutlemonde/Carte_Moisson/Facturation
-psql gestsea -f facoti.sql
+day=`date +%Y%m%d`
+psql gestsea -f ${d}/facoti.sql
 cp /tmp/FCM_*.pdf ${dir}
 cp /tmp/LCM_*.pdf ${dir}
-chmod 664 ${dir}/*.pdf
-rm /tmp/FCM_*.pdf /tmp/LCM_*.pdf
+cp /tmp/alvea_maj.csv ${dir}/alvea_maj_${day}.csv
+chmod 664 ${dir}/*
+rm /tmp/FCM_*.pdf /tmp/LCM_*.pdf /tmp/alvea_maj*
--- tasks/facoti.sql
+++ tasks/facoti.sql
@@ -1,7 +1,42 @@
-
 -- Création de la fonction
-
-
+CREATE OR REPLACE FUNCTION fc_finish_cotisations(IN started_on DATE, IN stopped_on DATE) RETURNS BOOLEAN AS
+$$
+DECLARE
+  f TEXT;
+BEGIN
+  PERFORM fc_cotisation_vers_facture(cs_numero) FROM cotisation WHERE not cs_done ORDER BY cs_numero;
+  SELECT fc_imprimelot(1,'facture',started_on, stopped_on) INTO f;
+  PERFORM execution('mv '||SUBSTR(f,8)||' /tmp/FCM_'||to_char(started_on,'YYYYMMDD')||'_'||to_char(stopped_on,'YYYYMMDD')||'.pdf');
+  SELECT fc_imprimelot(1,'carte',started_on, stopped_on) INTO f;
+  PERFORM execution('mv '||SUBSTR(f,8)||' /tmp/LCM_'||to_char(started_on,'YYYYMMDD')||'_'||to_char(stopped_on,'YYYYMMDD')||'.pdf');
+  RETURN true;
+END;
+$$ 
+LANGUAGE 'plpgsql';
 
 -- Mise en application de la fonction
+SELECT fc_finish_cotisations((CURRENT_DATE-'6 days'::INTERVAL)::DATE, CURRENT_DATE);
+
+DROP VIEW VUE_Cerealiers;
+
+CREATE OR REPLACE VIEW VUE_Cerealiers AS
+  SELECT c.pe_numero AS cs_personne, cs_societe, fa_numero, cs_numero FROM table_lignefacture join table_facture f using (fa_numero) JOIN table_cotisation c ON (f.pe_numero IN (c.pe_numero, c.cs_societe)) WHERE pd_numero=500000058 and extract(year from fa_date)=EXTRACT(YEAR FROM CURRENT_DATE);
+
+\f ;
+\a
+\t
+
+\o /tmp/alvea_maj.csv
+
+\qecho ;Personne;;;;;;;;;;;Société;;;;;;;;;;;
+\qecho Céréalier;N°;Titre;Nom;Prenom;Ligne_2;Ligne_3;Ligne_4;Ligne_5;CP;Ville;Telephone;N°; Titre;Nom;Prenom;Ligne_2;Ligne_3;Ligne_4;Ligne_5;CP;Ville;Telephone
+SELECT CASE WHEN c.cs_numero IN (SELECT cs_numero FROM VUE_Cerealiers) THEN 'X' ELSE '' END AS cereal, p.pe_id, p.pe_adresse, '="'||p.pe_telephone||'"', s.pe_id, s.pe_adresse, '="'||s.pe_telephone||'"'
+  FROM table_cotisation c join vue_personne p using (pe_numero) LEFT JOIN vue_personne s ON (c.cs_societe=s.pe_numero) 
+  WHERE cs_annee=EXTRACT(YEAR FROM CURRENT_DATE) AND (c.created_at::date BETWEEN (CURRENT_DATE-'6 days'::INTERVAL)::DATE AND CURRENT_DATE)
+ORDER BY 1 DESC, p.pe_nom
+;
+
+\t
+\a
+\f |
 
