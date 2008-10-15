a011e50d49bbae1498725936dc91236313558e04
Brice Texier <brice.texier@ekylibre.org>
2008-10-15 06:48:23 +0000

    git-svn-id: https://www.fdsea33.fr/svn/gestsea@103 06a6e951-9446-0410-82e0-8ca84c92daee

--- scripts/reverse/sql2xml.sql
+++ scripts/reverse/sql2xml.sql
@@ -13,9 +13,9 @@ DECLARE
   ku record;
   table_prefix text;
 BEGIN
-  xml:= '<?xml version="1.0" encoding="UTF-8"?>\n';
+  xml:= E'<?xml version="1.0" encoding="UTF-8"?>\n';
   SELECT current_database() INTO word;
-  xml:=xml||'<analysis name="'||current_database()||'">\n';
+  xml:=xml||E'<analysis name="'||current_database()||E'">\n';
 /*
   table_prefix := '';
   SELECT relname FROM pg_catalog.pg_class pc JOIN pg_catalog.pg_namespace pn ON (pc.relnamespace=pn.oid) WHERE nspname='public' AND relkind='r' INTO firstref;
@@ -29,37 +29,37 @@ BEGIN
 
   -- Recuperation des tables du schema public
   FOR t IN SELECT pc.oid AS pcrelid,* FROM pg_catalog.pg_class pc JOIN pg_catalog.pg_namespace pn ON (pc.relnamespace=pn.oid) WHERE nspname='public' AND relkind='r' ORDER BY relname LOOP
-    xml:=xml||'  <table name="'||t.relname||'" label="'||INITCAP(REPLACE(t.relname,'_',' '))||'">\n';
+    xml:=xml||E'  <table name="'||t.relname||E'" label="'||INITCAP(REPLACE(t.relname,E'_',E' '))||E'">\n';
     FOR c IN SELECT * FROM pg_class pc JOIN pg_attribute pa ON (pa.attrelid=pc.oid) JOIN pg_type pt ON (pt.oid = pa.atttypid) LEFT JOIN pg_attrdef ad ON (pc.oid=ad.adrelid AND adnum=attnum) WHERE pc.relname=t.relname AND pa.attnum > 0 AND NOT pa.attisdropped LOOP
-      xml:=xml||'    <column name="'||c.attname||'" label="'||INITCAP(REPLACE(c.attname,'_',' '))||'"';
-      xml:=xml||' type="'||UPPER(c.typname);
+      xml:=xml||E'    <column name="'||c.attname||E'" label="'||INITCAP(REPLACE(c.attname,E'_',E' '))||E'"';
+      xml:=xml||E' type="'||UPPER(c.typname);
 
       IF c.atttypmod>0 THEN
-        xml:=xml||'('||c.atttypmod-4||')';
+        xml:=xml||E'('||c.atttypmod-4||E')';
       END IF;
 /*
-      ELSIF c.udt_name='numeric' AND c.numeric_precision IS NOT NULL THEN
-        xml:=xml||'('||c.numeric_precision;
+      ELSIF c.udt_name=E'numeric' AND c.numeric_precision IS NOT NULL THEN
+        xml:=xml||E'('||c.numeric_precision;
         IF c.numeric_scale > 0 THEN
-          xml:=xml||','||COALESCE(c.numeric_scale,0);
+          xml:=xml||E','||COALESCE(c.numeric_scale,0);
         END IF;
-        xml:=xml||')';
+        xml:=xml||E')';
       END IF;
 */
-      xml:=xml||'"';
+      xml:=xml||E'"';
       IF c.attnotnull THEN
-        xml:=xml||' notnull="true"';
+        xml:=xml||E' notnull="true"';
       END IF;
       IF c.atthasdef THEN
-        xml:=xml||' default="'||c.adsrc||'"';
+        xml:=xml||E' default="'||c.adsrc||E'"';
       END IF;
 
       FOR ct IN SELECT * from pg_constraint where conrelid=t.pcrelid AND c.attnum = ANY(conkey) LOOP
-        IF ct.contype='p' THEN
-          xml:=xml||' pkey="true"';
-        ELSIF ct.contype='f' THEN
-          SELECT COALESCE(pc.relname||'('||pa.attname||')','72') FROM pg_attribute pa JOIN pg_class pc ON (attrelid=pc.oid) WHERE pc.oid=ct.confrelid AND attnum=ct.confkey[1] INTO word;
-          xml:=xml||' fkey="'||word||'" opt="ou'||ct.confupdtype||'od'||ct.confdeltype||'"';
+        IF ct.contype=E'p' THEN
+          xml:=xml||E' pkey="true"';
+        ELSIF ct.contype=E'f' THEN
+          SELECT COALESCE(pc.relname||E'('||pa.attname||E')',E'72') FROM pg_attribute pa JOIN pg_class pc ON (attrelid=pc.oid) WHERE pc.oid=ct.confrelid AND attnum=ct.confkey[1] INTO word;
+          xml:=xml||E' fkey="'||word||E'" opt="ou'||ct.confupdtype||E'od'||ct.confdeltype||E'"';
         END IF;
       END LOOP;
 /*
@@ -67,18 +67,18 @@ BEGIN
         SELECT * FROM information_schema.key_column_usage WHERE constraint_name=ct.constraint_name INTO ku;
         IF ku.position_in_unique_constraint IS NULL THEN
           -- Primary Key
-          xml:=xml||' pkey="true"';
+          xml:=xml||E' pkey="true"';
         ELSE
           -- Foreign key
-          xml:=xml||' fkey="'||ku.table_name||'('||ku.column_name||')"';
+          xml:=xml||E' fkey="'||ku.table_name||E'('||ku.column_name||E')"';
         END IF;
       END LOOP;
 */
-      xml:=xml||'/>\n';
+      xml:=xml||E'/>\n';
     END LOOP;
-    xml:=xml||'  </table>\n\n';
+    xml:=xml||E'  </table>\n\n';
   END LOOP;
-  xml:=xml||'</analysis>\n';
+  xml:=xml||E'</analysis>\n';
   RETURN xml;
 END;
 $$
